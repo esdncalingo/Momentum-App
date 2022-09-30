4529f93be6183cd9f01daf2684cdb0a1ac8d5bb8
Jastine Marano <jpmarano@up.edu.ph>
2022-09-30 12:47:30 +0800

    Modified multiple files

--- js/momentumscript.js
+++ js/momentumscript.js
@@ -81,37 +81,3 @@ maintainUserName();
 userName.addEventListener('blur', () => {
   userName.innerHTML = localStorage.getItem('newname');
 })
-
-//settings main click
-
-const settingsMain = document.querySelector('.settings-img');
-const settingsToggle = document.querySelector('.settings-popup');
-
-settingsMain.addEventListener('click', () => {
-    settingsToggle.classList.toggle('active');
-})
-
-
-//QuoteGenerator
-var quotes = 
-[`“Tough times don’t last. Tough people do.”`,
-`“It does not matter how slowly you go as long as you do not stop.”`,
-`“Everything you’ve ever wanted is on the other side of fear.”`,
-`“Pain is temporary. Quitting lasts forever.”`,
-`“A problem is a chance for you to do your best.”`,
-`“Hard times don’t create heroes. It is during the hard times when the ‘hero’ within us is revealed.”`,
-`“It’s not about perfect. It’s about effort.”`,
-`“Believe you can and you’re halfway there.”`,
-`“Difficult roads always lead to beautiful destinations.”`,
-`“Your time is limited, so don’t waste it living someone else’s life.”`,
-`“Perseverance is failing 19 times and succeeding the 20th.”`,
-`“Goals may give focus, but dreams give power.”`,
-`“You cannot fail at being yourself.”`,
-`“You cannot fail at being yourself.”`,
-`“If you cannot do great things, do small things in a great way.”`,
-`“When you know what you want, and want it bad enough, you’ll find a way to get it.”`];
-
-
-var randNumber = Math.floor(Math.random()*(quotes.length));
-document.getElementById("display-quotes").innerHTML = quotes[randNumber];
-
