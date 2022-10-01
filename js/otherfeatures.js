f0f98222e327cc5e7cd1a1c270877732fbc1b90a
esdncalingo <esdncalingo@gmail.com>
2022-10-02 00:15:51 +0800

    fixed overlaping elements

--- js/otherfeatures.js
+++ js/otherfeatures.js
@@ -5,11 +5,32 @@ const settingsMain = document.querySelector('.settings-img');
 const settingsToggle = document.querySelector('.settings-popup');
 
 settingsMain.addEventListener('click', () => {
-    settingsToggle.classList.toggle('active');
+    settingsToggle.classList.toggle("active");
+})
+
+//Add a quote button click
+
+const quoteBtn = document.querySelector('.ellipsis-vertical');
+const insertQuote = document.querySelector('.insertquote-popup');
+
+quoteBtn.addEventListener('click',() => {
+    insertQuote.classList.toggle("active");
+})
+
+//Cancel add a quote button click
+
+
+const quoteCancel = document.querySelector('close-btn');
+const quotePopup = document.querySelector('.insertquote-popup');
+
+quoteCancel.addEventListener('click',() => {
+    quotePopup.classList.remove('active');
 })
 
 
+
 //QuoteGenerator
+function randomQuote () {
 var quotes = 
 [`“Tough times don’t last. Tough people do.”`,
 `“It does not matter how slowly you go as long as you do not stop.”`,
@@ -27,8 +48,22 @@ var quotes =
 `“You cannot fail at being yourself.”`,
 `“If you cannot do great things, do small things in a great way.”`,
 `“When you know what you want, and want it bad enough, you’ll find a way to get it.”`];
+ var randNumber = Math.floor(Math.random()*(quotes.length));
+ document.getElementById("display-quotes").innerHTML = quotes[randNumber];
+};
 
+//Add a quote
+function newQuote(){
+    const addedQuote = document.getElementById("insertquote").value;
+    document.getElementById("display-quotes").innerHTML = `"${addedQuote}"`;
+};
 
-var randNumber = Math.floor(Math.random()*(quotes.length));
-document.getElementById("display-quotes").innerHTML = quotes[randNumber];
+//Delete field text
 
+function deleteText(){
+
+    if(document.getElementById("insertquote").value !== ""){
+        document.getElementById("insertquote").value="";}
+    else {document.getElementById("insertquote").value=" ";
+    } 
+};
